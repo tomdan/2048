fd07b9eb045d52ad0644c6929b030fd15c53a9bc
tomdan94 <tomdan94@gmail.com>
2020-10-05 15:51:37 +0200

    added powerOfTwo attribute to Tile class

--- src/Tile.java
+++ src/Tile.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.lang.Math;
 
 public class Tile extends JLabel {
 	
 	static final int TILE_WIDTH = 50;
     static final int TILE_HEIGHT = 50;
+    
+    private int powerOfTwo;
     
     Tile(){
     	this.setSize(TILE_WIDTH, TILE_HEIGHT);
@@ -13,6 +16,23 @@ public class Tile extends JLabel {
         this.setHorizontalAlignment(CENTER);
         this.setBackground(Color.BLACK);
         this.setOpaque(true);
+        
+        this.setPowerOfTwo(0);
     }
+
+	public int getPowerOfTwo() {
+		return powerOfTwo;
+	}
+
+	public void setPowerOfTwo(int powerOfTwo) {
+		this.powerOfTwo = powerOfTwo;
+		
+		// set the correct text on the tile label
+		if (powerOfTwo == 0) {
+            this.setText(" ");
+        } else {
+            this.setText(Double.toString(Math.pow(2,powerOfTwo)));
+        }
+	}
     
 }
