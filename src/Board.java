41e44f957c6380f1f9d21321311eeb7b929eec28
tomdan94 <tomdan94@gmail.com>
2020-10-05 16:54:29 +0200

    added a function that puts a new tile on the game board

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Board extends JPanel {
 	
@@ -10,6 +11,8 @@ public class Board extends JPanel {
     
     // Tiles on the board - Tiles[column][row]
     Tile[][] tiles = new Tile[BOARD_SIZE][BOARD_SIZE];
+    
+    Random random_number = new Random();
     
 	Board() {
 
@@ -23,5 +26,42 @@ public class Board extends JPanel {
                 this.add(tiles[j][i]);
             }
         }
+        
+        this.put2or4Tile();
+        this.put2or4Tile();
 	}
+	
+	//this function puts a new tile
+	public void put2or4Tile() {
+        int row = 0;
+        int column = 0;
+        int x2or4 = random_number.nextInt(100);
+        int counter = 0;
+
+        // the counter is the number of empty tiles
+        for (int c = 0; c < BOARD_SIZE; c++) {
+            for (int r = 0; r < BOARD_SIZE; r++) {
+                if (tiles[c][r].getPowerOfTwo() == 0) {
+                    counter++;
+                }
+            }
+        }
+
+        // 80% tile = 2, 20% tile = 4
+        if (x2or4 < 80) {
+            x2or4 = 1;
+        } else {
+            x2or4 = 2;
+        }
+
+        // if the game board has any empty tile, this function puts a new tile
+        if (counter > 0) {
+            do {
+                row = random_number.nextInt(BOARD_SIZE);
+                column = random_number.nextInt(BOARD_SIZE);
+            } while (tiles[column][row].getPowerOfTwo() != 0);
+            tiles[column][row].setPowerOfTwo(x2or4);
+        }
+    }
+	
 }
