b3a65c8222ae9f564c3f94e43ed387df50477437
tomdan94 <tomdan94@gmail.com>
2020-10-06 16:43:15 +0200

    added menu panel

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -6,7 +6,13 @@ public class GameFrame extends JFrame {
     static final int FRAME_WIDTH = 400;
     static final int FRAME_HEIGHT = 400;
     
+    static final int MENU_PANEL_WIDTH = 50;
+    static final int MENU_PANEL_HEIGHT = 50;
+    static final Color MENU_PANEL_COLOR = new Color(200, 200, 200);
+    
     ImageIcon myFrameIcon = new ImageIcon("img/icon.jpg");
+    
+    JPanel menuPanel = new JPanel();
     
     GameFrame() {
 
@@ -16,8 +22,14 @@ public class GameFrame extends JFrame {
         this.setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
         this.setIconImage(myFrameIcon.getImage());
         
+        // MENU PANEL
+        menuPanel.setPreferredSize(new Dimension(MENU_PANEL_WIDTH, MENU_PANEL_HEIGHT));
+        menuPanel.setLayout(null);
+        menuPanel.setBackground(MENU_PANEL_COLOR);
+        
         // FRAME COMPONENTS
-        this.add(new Board());
+        this.add(menuPanel, BorderLayout.NORTH);
+        this.add(new Board(), BorderLayout.CENTER);
         
         // FRAME VISIBLE AND CENTER
         this.setVisible(true);
