dcff54eecc2c6b55d6c8853d3e01e1e9896c5290
tomdan94 <tomdan94@gmail.com>
2020-10-06 19:26:50 +0200

    added new game button

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class GameFrame extends JFrame {
+public class GameFrame extends JFrame implements ActionListener {
 
     static final int FRAME_WIDTH = 400;
     static final int FRAME_HEIGHT = 400;
@@ -12,7 +13,10 @@ public class GameFrame extends JFrame {
     
     ImageIcon myFrameIcon = new ImageIcon("img/icon.jpg");
     
+    // COMPONENTS
+    Board gameBoard = new Board();
     JPanel menuPanel = new JPanel();
+    JButton newGameButton = new JButton("Nowa gra");
     
     GameFrame() {
 
@@ -22,18 +26,33 @@ public class GameFrame extends JFrame {
         this.setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
         this.setIconImage(myFrameIcon.getImage());
         
+        // NEW GAME BUTTON
+        newGameButton.setBounds(10, 10, 150, 30);
+        newGameButton.setFocusable(false);
+        newGameButton.setFont(new Font("MV Boli", Font.ROMAN_BASELINE, 20));
+        newGameButton.addActionListener(this);
+        
         // MENU PANEL
         menuPanel.setPreferredSize(new Dimension(MENU_PANEL_WIDTH, MENU_PANEL_HEIGHT));
         menuPanel.setLayout(null);
         menuPanel.setBackground(MENU_PANEL_COLOR);
+        menuPanel.add(newGameButton);
         
         // FRAME COMPONENTS
         this.add(menuPanel, BorderLayout.NORTH);
-        this.add(new Board(), BorderLayout.CENTER);
+        this.add(gameBoard, BorderLayout.CENTER);
         
         // FRAME VISIBLE AND CENTER
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         
     }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == newGameButton) {
+            gameBoard.startNewGame();
+        }
+    }
+    
 }
