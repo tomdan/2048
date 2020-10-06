572a17f35585cc94a97616deb2c4ef44fb402fa2
tomdan94 <tomdan94@gmail.com>
2020-10-06 13:53:58 +0200

    added new frame icon image

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -6,12 +6,15 @@ public class GameFrame extends JFrame {
     static final int FRAME_WIDTH = 400;
     static final int FRAME_HEIGHT = 400;
     
+    ImageIcon myFrameIcon = new ImageIcon("img/icon.jpg");
+    
     GameFrame() {
 
         // FRAME OPTIONS
         this.setTitle("My 2048 game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+        this.setIconImage(myFrameIcon.getImage());
         
         // FRAME COMPONENTS
         this.add(new Board());
@@ -20,9 +23,5 @@ public class GameFrame extends JFrame {
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         
-        
-        
-        
-
     }
 }
