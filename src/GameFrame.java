2f270be9168322a506cd4187cde2a9b650fa43db
tomdan94 <tomdan94@gmail.com>
2020-10-12 13:25:54 +0200

    added textLabel

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -8,7 +8,7 @@ public class GameFrame extends JFrame implements ActionListener {
     static final int FRAME_HEIGHT = 400;
 
     static final int MENU_PANEL_WIDTH = 50;
-    static final int MENU_PANEL_HEIGHT = 50;
+    static final int MENU_PANEL_HEIGHT = 70;
     static final Color MENU_PANEL_COLOR = new Color(200, 200, 200);
 
     ImageIcon myFrameIcon = new ImageIcon("img/icon.jpg");
@@ -18,6 +18,7 @@ public class GameFrame extends JFrame implements ActionListener {
     JPanel menuPanel = new JPanel();
     JButton newGameButton = new JButton("Nowa gra");
     JLabel scoreLabel = new JLabel("0");
+    JLabel textLabel = new JLabel(" ");
 
     ActionListener task = new ActionListener() {
         public void actionPerformed(ActionEvent evt) {
@@ -33,6 +34,12 @@ public class GameFrame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
         this.setIconImage(myFrameIcon.getImage());
+
+        // TEXT LABEL
+        textLabel.setForeground(Color.BLACK);
+        textLabel.setBounds(10, 40, 150, 30);
+        textLabel.setFont(new Font("MV Boli", Font.ROMAN_BASELINE, 20));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
 
         // SCORE LABEL
         scoreLabel.setForeground(Color.BLACK);
@@ -52,6 +59,7 @@ public class GameFrame extends JFrame implements ActionListener {
         menuPanel.setBackground(MENU_PANEL_COLOR);
         menuPanel.add(newGameButton);
         menuPanel.add(scoreLabel);
+        menuPanel.add(textLabel);
 
         // FRAME COMPONENTS
         this.add(menuPanel, BorderLayout.NORTH);
